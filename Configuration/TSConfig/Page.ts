44c509714f3463cdcf46feda20eccd6fdc9e7ee7
Sibylle Naegle <naegle@sub.uni-goettingen.de>
2014-10-21 13:27:43 +0200

    Initial, a hello world with language switch

---
+++ Configuration/TSConfig/Page.ts
@@ -0,0 +1,25 @@
+TCEFORM.tt_content {
+}
+
+TCEMAIN {
+}
+
+TCAdefaults {
+}
+
+# configuration for backend modules
+mod {
+}
+
+# RTE configuration
+RTE.default {
+}
+
+RTE.config.tt_content.bodytext.proc.allowedClasses < RTE.default.proc.allowedClasses
+
+RTE.classes {
+}
+
+#frames for columns
+TCEFORM.tt_content.section_frame {
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Requirements
+
+* Installed system extension
+	** T3jQuery
+	** vhs
+
+* Page TypoScript
+	** <INCLUDE_TYPOSCRIPT: source="FILE:EXT:tmpl_ipoa/Configuration/TypoScript/Static/setup.txt">
