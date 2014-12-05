4b077a466011d831403532eb19ea7d11fa20293c
Sibylle Naegle <naegle@sub.uni-goettingen.de>
2014-12-05 16:22:35 +0100

    [BUGFIX] IPOA-bug-se remove collision with solr extension

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,16 +2,4 @@
 if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
-
-return array(
-	'SYS' => array(
-		'displayErrors' => 1,
-		'enableDeprecationLog' => FALSE,
-		'isInitialInstallationInProgress' => FALSE,
-		'sqlDebug' => 0,
-		'systemLogLevel' => 2,
-		't3lib_cs_convMethod' => 'mbstring',
-		't3lib_cs_utils' => 'mbstring',
-		'trustedHostsPattern' => 'ipoa.dev',
-	),
-);
+?>
