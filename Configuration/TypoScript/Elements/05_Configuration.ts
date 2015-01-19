4034482d77ac42035c27eaf15e3488ef63e986fa
Sibylle Naegle <naegle@sub.uni-goettingen.de>
2015-01-19 16:22:16 +0100

    [TASK] IPOA-370 style CE tables

--- Configuration/TypoScript/Elements/05_Configuration.ts
+++ Configuration/TypoScript/Elements/05_Configuration.ts
@@ -1,6 +1,16 @@
-config.absRefPrefix = /
-config.no_cache = 1
-config.doctype = html5
+
+
+config {
+	# Add class to html-Tag
+	#htmlTag_setParams = lang="de" class="js"
+	htmlTag_setParams = lang="de" class="js" xmlns="http://www.w3.org/1999/xhtml"
+
+	#add doctype
+	doctype = html5
+
+	absRefPrefix = /
+	no_cache = 1
+}
 
 config {
 	linkVars = L
@@ -26,6 +36,8 @@ styles.content.imgtext.layoutKey = srcset
 		locale_all = en_US.utf8
 		sys_language_mode = content_fallback
 		sys_language_overlay = 1
+		# change setting in html-tag
+		htmlTag_setParams = lang="en" class="js" xmlns="http://www.w3.org/1999/xhtml"
 	}
 [global]
 [globalVar = GP:L = 2]
@@ -36,6 +48,8 @@ styles.content.imgtext.layoutKey = srcset
 		locale_all = de_CH.utf8
 		sys_language_mode = content_fallback
 		sys_language_overlay = 1
+		# change setting in html-tag
+		htmlTag_setParams = lang="ch" class="js" xmlns="http://www.w3.org/1999/xhtml"
 	}
 [global]
 [globalVar = GP:L = 3]
@@ -46,6 +60,8 @@ styles.content.imgtext.layoutKey = srcset
 		sys_language_mode = content_fallback; 2,1,0
 		sys_language_fallBackOrder = 2,1,0
 		sys_language_overlay = 1
+		# change setting in html-tag
+		htmlTag_setParams = lang="en" class="js" xmlns="http://www.w3.org/1999/xhtml"
 	}
 [global]
 [globalVar = GP:L = 4]
@@ -56,6 +72,8 @@ styles.content.imgtext.layoutKey = srcset
 		sys_language_mode = content_fallback; 3,2,1,0
 		sys_language_fallBackOrder = 3,2,1,0
 		sys_language_overlay = 1
+		# change setting in html-tag
+		htmlTag_setParams = lang="fr" class="js" xmlns="http://www.w3.org/1999/xhtml"
 	}
 [global]
 [globalVar = GP:L = 5]
@@ -65,6 +83,8 @@ styles.content.imgtext.layoutKey = srcset
 		locale_all = de_AT.utf8
 		sys_language_mode = content_fallback
 		sys_language_overlay = 1
+		# change setting in html-tag
+		htmlTag_setParams = lang="de" class="js" xmlns="http://www.w3.org/1999/xhtml"
 	}
 [global]
 [globalVar = GP:L = 6]
@@ -75,5 +95,28 @@ styles.content.imgtext.layoutKey = srcset
 		sys_language_mode = content_fallback; 5,1,0
 		sys_language_fallBackOrder = 5,1,0
 		sys_language_overlay = 1
+		# change setting in html-tag
+		htmlTag_setParams = lang="en" class="js" xmlns="http://www.w3.org/1999/xhtml"
 	}
 [global]
+
+page.includeCSS {
+	file100 = http://subugoe.github.io/ipoa-html-template/css/style.css
+}
+# include all necessary js-files at the end of the page, if possible
+page.javascriptLibs {
+	jQuery = 1
+	jQuery.version = latest
+	jQuery.source = local
+}
+# include some javascript in header
+page.includeJS {
+	file100 = typo3conf/ext/tmpl_ipoa/Resources/Public/Javascript/picturefill.min.js
+}
+# include some javascript in footer
+page.includeJSFooter {
+	file100 = typo3conf/ext/tmpl_ipoa/Resources/Public/Javascript/jquery.fittext.js
+	file200 = typo3conf/ext/tmpl_ipoa/Resources/Public/Javascript/headroom.min.js
+	file300 = typo3conf/ext/tmpl_ipoa/Resources/Public/Javascript/jQuery.headroom.min.js
+	file400 = typo3conf/ext/tmpl_ipoa/Resources/Public/Javascript/ipoa.js
+}
