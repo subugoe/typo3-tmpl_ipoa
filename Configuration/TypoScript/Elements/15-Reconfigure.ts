dad299d5cf7baa4316e50b7d016dc75902149744
Sibylle Naegle <naegle@sub.uni-goettingen.de>
2015-01-20 10:44:50 +0100

    [TASK] reformat code for better reading

--- Configuration/TypoScript/Elements/15-Reconfigure.ts
+++ Configuration/TypoScript/Elements/15-Reconfigure.ts
@@ -1,5 +1,5 @@
 ##############
-# Clean up content
+# Clean up and change content
 
 
 # Add class to body-Tag
@@ -22,42 +22,48 @@ tt_content.stdWrap.innerWrap >
 lib.stdheader.stdWrap.dataWrap >
 lib.stdheader.3.headerClass >
 
+# add class to title of content element
+# that it is h2 instead of h1 is written in PageTs Constants right now (see above)
+lib.stdheader.10.2.dataWrap = <h2 class="main__heading">|</h2>
 
-##############
-# change tag of content element
-##############
 
 # unfortunately I haven't found a way to change the defaulttag to h2 from inside the extension
 # the following line doesn't work
 #content.defaultHeaderType = 2
 
-# add class to title of content element
-# that it is h2 instead of h1 is written in PageTs Constants right now (see above)
-lib.stdheader.10.2.dataWrap = <h2 class="main__heading">|</h2>
+tt_content {
+	text {
+		20 {
+			# wrap text-content in special div
+			stdWrap.dataWrap = <div class="main__textblock">|</div>
 
-# wrap text-content in special div
-tt_content.text.20.stdWrap.dataWrap = <div class="main__textblock">|</div>
-
-# add wrap for table content element
-tt_content.table.20.stdWrap.wrap = <div class="main__table">|</div>
-
-tt_content.table.20.stdWrap.replacement {
-	#remove default classes of table
-	# (the class contenttable cause the css breadcrumbs to change from html.js body.body.content to html body)
-	10 {
-	    search = class="contenttable contenttable-0"
-	    replace =
-	  }
-	# wrap main__link around links in table
-	30 {
-		search = a href
-		replace = a class="main__link" href
+			replacement {
+				# wrap main__link around links in text
+				10 {
+					search = a href
+					replace = a class="main__link" href
+				}
+			}
+		}
 	}
-}
-tt_content.text.20.replacement {
-	# wrap main__link around links in text
-	10 {
-		search = a href
-		replace = a class="main__link" href
+	table {
+		20 {
+			#add wrap for table content element
+			stdWrap.wrap = <div class="main__table">|</div>
+
+			stdWrap.replacement {
+				#remove default classes of table
+				# (the class contenttable cause the css breadcrumbs to change from html.js body.body.content to html body)
+				10 {
+					search = class="contenttable contenttable-0"
+					replace =
+				}
+				# wrap main__link around links in table
+				30 {
+					search = a href
+					replace = a class="main__link" href
+				}
+			}
+		}
 	}
 }
