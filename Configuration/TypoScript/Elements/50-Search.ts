19d81acdca0fe5fc0523767157900cb0a9657065
Sibylle Naegle <naegle@sub.uni-goettingen.de>
2015-03-12 13:35:26 +0100

    [TASK] remove default search form from solr plugin

---
+++ Configuration/TypoScript/Elements/50-Search.ts
@@ -0,0 +1,24 @@
+plugin.tx_solr {
+
+	templateFiles {
+		frequentSearches = EXT:tmpl_ipoa/Resources/Private/Solr/Resources/Private/Templates/PiFrequentSearches/frequentsearches.htm
+		pagebrowser = EXT:tmpl_ipoa/Resources/Private/Solr/Resources/Private/Templates/PiResults/pagebrowser.htm
+		results = EXT:tmpl_ipoa/Resources/Private/Solr/Resources/Private/Templates/PiResults/results.htm
+		search = EXT:tmpl_ipoa/Resources/Private/Solr/Resources/Private/Templates/PiSearch/search.htm
+	}
+}
+
+plugin.tx_solr {
+	templateFiles {
+		frequentSearches = EXT:tmpl_ipoa/Resources/Private/Solr/Resources/Private/Templates/PiFrequentSearches/frequentsearches.htm
+		pagebrowser = EXT:tmpl_ipoa/Resources/Private/Solr/Resources/Private/Templates/PiResults/pagebrowser.htm
+		results = EXT:tmpl_ipoa/Resources/Private/Solr/Resources/Private/Templates/PiResults/results.htm
+		search = EXT:tmpl_ipoa/Resources/Private/Solr/Resources/Private/Templates/PiSearch/search.htm
+	}
+
+	cssFiles {
+		results     = EXT:tmpl_ipoa/Resources/Private/Solr/Resources/Private/Css/PiResults/results.css
+		pagebrowser = EXT:pagebrowse/res/styles_min.css
+		ui          = EXT:solr/Resources/Css/JQueryUi/jquery-ui.custom.css
+	}
+}
