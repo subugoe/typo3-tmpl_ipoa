efd7b29a5c250d6c3151a249522f4cc0fcae9ae2
Sibylle Naegle <naegle@sub.uni-goettingen.de>
2015-03-30 14:42:19 +0200

    [TASK] add language selection to start page

--- Configuration/TypoScript/Elements/40-AdditionalConfiguration.ts
+++ Configuration/TypoScript/Elements/40-AdditionalConfiguration.ts
@@ -1,17 +1,10 @@
 
 # include css file depending on pagetree
-[PIDinRootline = {$ids.start}]
-	page.includeCSS.file103 = typo3conf/ext/tmpl_ipoa/Resources/Public/Css/start.css
-[else]
-	page.includeCSS.file103 = typo3conf/ext/tmpl_ipoa/Resources/Public/Css/style.css
-	page.headerData.1027 = TEXT
-	page.headerData.1027.value (
-		 <script src="/typo3conf/ext/tmpl_ipoa/Resources/Public/Javascript/ipoa.js" type="text/javascript"></script>
-	)
-[global]
-page.headerData.111 = TEXT
-page.headerData.111.value (
-	<meta content="width=device-width, initial-scale=1" name="viewport">
+page.includeCSS.file103 = typo3conf/ext/tmpl_ipoa/Resources/Public/Css/style.css
+
+page.headerData.1027 = TEXT
+page.headerData.1027.value (
+	 <script src="/typo3conf/ext/tmpl_ipoa/Resources/Public/Javascript/ipoa.js" type="text/javascript"></script>
 )
 
 page.headerData.800 = TEXT
