0f92541d15da11168f8f51a16625961908cfe561
Sibylle Naegle <naegle@sub.uni-goettingen.de>
2016-01-27 12:02:17 +0100

    IPOA-855 remove menu for dynamic error page

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,10 +1,12 @@
 <?php
+
+
 if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
 // 404 handling:
-// in case of page not found, redirect depening on URL
-$TYPO3_CONF_VARS['FE']['pageNotFound_handling'] = 'USER_FUNCTION:typo3conf/ext/tmpl_ipoa/Classes/pageNotFoundHandling.php:user_pageNotFound->pageNotFound';
+$TYPO3_CONF_VARS['FE']['pageNotFound_handling'] = '/Uuups,%20diese%20Seite%20existiert%20nicht/';
 $TYPO3_CONF_VARS['FE']['pageNotFound_handling_statheader'] = "HTTP/1.1 404 Not Found";
+
 ?>
