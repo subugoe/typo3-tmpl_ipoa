edaad06beeb97f049d2f6713797abbb8832c6c1c
snaegle <snaegle@users.noreply.github.com>
2018-10-16 08:15:31 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'SUB Uni-Goettingen',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-8.7.99',
+            'typo3' => '8.7.0-9.9.99',
         ],
     ],
 ];
