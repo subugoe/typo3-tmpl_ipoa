7ca21098c13213cc2fac3cb00d582d88bcc3c5fe
snaegle <naegle@sub.uni-goettingen.de>
2019-03-30 19:53:21 +0100

    Fix english language and implement new design for lang button
    Related to: OAFWM-52

--- Resources/Public/JavaScript/ipoa.js
+++ Resources/Public/JavaScript/ipoa.js
@@ -317,34 +317,3 @@ jQuery(function () {
     loadMenu();
   }, 10);
 });
-
-jQuery(function () {
-
-  /**
-   * Language menu
-   * at first, it is hidden, but when the "language-indicator" is clicked,
-   * it is shown and the "language-indicator" is hidden
-   * This menu exists twice:
-   * - once in the header of content pages
-   * - once on the start screen
-   */
-  const languageMenuShow = function (place) {
-    jQuery('.js-' + place + '-language-links').removeClass('-shown').addClass('-hidden');
-    const jsLanguageIndicator = jQuery('.js-' + place + '-language-indicator');
-
-    jsLanguageIndicator.click(function () {
-      jQuery('.js-' + place + '-language-links').removeClass('-hidden').addClass('-shown');
-      const langHeight = jQuery('.' + place + '__links').height();
-      jQuery('.' + place).css({ 'padding-top': langHeight });
-      jsLanguageIndicator.removeClass('-shown').addClass('-hidden');
-    });
-  };
-
-  /**
-   * There are two language menus on the web page
-   * - on startpage and
-   * - on all other pages in the head
-   */
-  languageMenuShow('head');
-  languageMenuShow('start');
-});
