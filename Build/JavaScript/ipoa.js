81bc1c601cad720897db51deefaedd197c3951c4
snaegle1 <naegle@sub.uni-goettingen.de>
2020-06-12 08:39:34 +0200

    Add bootstrap buttons to start page

--- Build/JavaScript/ipoa.js
+++ Build/JavaScript/ipoa.js
@@ -1,37 +1,39 @@
+"use strict";
+
 jQuery(() => {
   /**
    * scroll to top when page is loaded
    */
-  jQuery('html,body').animate({ 'scrollTop': 0 }, 500);
-
+  jQuery('html,body').animate({
+    'scrollTop': 0
+  }, 500);
   /**
    * this part allows for in-page anchor clicks that move the scroll position
    * without wrongly covering the content by the top nav bar.
    *
    * 1. TODO : make dirty number clean
    */
+
   jQuery('.main div a').click(function () {
     const scrollToId = this.hash;
     const offsetTopOfTarget = jQuery(scrollToId).offset().top;
-    jQuery('html, body').scrollTop(offsetTopOfTarget - 65);
-    // 1.
+    jQuery('html, body').scrollTop(offsetTopOfTarget - 65); // 1.
   });
-
   /**
    * change the top menu bar according to scroll status
    */
+
   jQuery(() => {
     let lastScrollTop = 0;
     const delta = 5;
-
     jQuery(window).scroll(function () {
       const st = jQuery(this).scrollTop();
 
       if (Math.abs(lastScrollTop - st) <= delta) {
         return;
-      }
+      } // downscroll code
 
-      // downscroll code
+
       if (st > lastScrollTop) {
         jQuery('.js-head-language-links').hide();
         jQuery('.js-head-language-indicator').show();
@@ -46,9 +48,7 @@ jQuery(() => {
     });
   });
 });
-
 jQuery(() => {
-
   const menuToggleChildren = function (item) {
     jQuery(item).siblings('.menu__list--indented').toggle();
 
@@ -59,54 +59,53 @@ jQuery(() => {
       jQuery(item).find('svg').attr('class', 'fa-icon fa-icon-angle-double-down');
       jQuery(item).find('svg').html('<use xlink:href="#icon-angle-double-down"></use>');
     }
-  };
-
-  // add menu per ajax:
+  }; // add menu per ajax:
   // load answer of ajax-page directly into the element, then execute function
-  const animateMenu = function () {
 
+
+  const animateMenu = function () {
     // Add svg instances to svg tag as it can't be done from typoscript,
     // because the menu is created with a different typeNum without javascript on
     jQuery('.toggle-menu').find('.fa-icon-angle-double-right').html('<use xlink:href="#icon-angle-double-right"></use>');
-    jQuery('.toggle-menu').find('.fa-icon-angle-double-down').html('<use xlink:href="#icon-angle-double-down"></use>');
-
-    // Now, that the DOM-Elements are available, their behaviour can be configured:
+    jQuery('.toggle-menu').find('.fa-icon-angle-double-down').html('<use xlink:href="#icon-angle-double-down"></use>'); // Now, that the DOM-Elements are available, their behaviour can be configured:
     // Configure toggle-icons (the arrows left of menu items)
     // every arrow-icon in the menu gets to toggle the respective submenu
+
     jQuery('.toggle-menu').click(function () {
       menuToggleChildren(this);
-    });
-
-    // detect keyboard /clicks/ on menu toggle buttons
+    }); // detect keyboard /clicks/ on menu toggle buttons
     // keycode 13 = return/enter key
+
     jQuery('.toggle-menu').focus().keydown(function (event) {
       if (event.keyCode === 13) {
         menuToggleChildren(this);
       }
-    });
+    }); // when arrow is clicked, move menu up or down
 
-    // when arrow is clicked, move menu up or down
     const menu = jQuery('.js-menu');
     const menuToggleButton = jQuery('.js-menu-toggle-button');
-
     jQuery(menuToggleButton).click(event => {
-      jQuery('html,body').animate({ 'scrollTop': 0 }, 500);
+      jQuery('html,body').animate({
+        'scrollTop': 0
+      }, 500);
       jQuery(menu).slideUp();
       event.preventDefault();
-    });
-
-    // hide all sub menu lists except for the current one
+    }); // hide all sub menu lists except for the current one
     // for no-js fallback cases they are open by default, so we have to explicitly
     // close them
+
     jQuery('.alt-menu .menu__list--indented').hide();
     jQuery('.fa-icon-angle-double-down').parent('span').siblings('.menu__list--indented').show();
-    jQuery('.alt-menu__content').animate({ 'scrollTop': 0 }, 500);
+    jQuery('.alt-menu__content').animate({
+      'scrollTop': 0
+    }, 500);
   };
-  animateMenu();
 
+  animateMenu();
   /**
    * handle display of menu
    */
+
   const menuIsActive = function () {
     return jQuery('.alt-menu').hasClass('alt-menu-in-canvas');
   };
@@ -125,36 +124,34 @@ jQuery(() => {
     } else {
       showMenu();
     }
-  });
+  }); // make sure, only active language is visible - except for decision
 
-  // make sure, only active language is visible - except for decision
   jQuery('.head__link-language').on('click', () => {
     jQuery('.head__link-language:not(.head__link-active)').toggleClass('-shown');
   });
-
   /**
    * handle on demand content on pages like those for the oa days
    */
+
   jQuery('.on-demand__content').addClass('').hide();
   jQuery('.on-demand__link').click(function () {
     jQuery(this).parent('p').nextUntil(':not(".on-demand__content")').toggle('slow');
   });
-
   jQuery(window).resize(() => {
     hideMenu();
+
     if (jQuery(window).width() > 820) {
       jQuery('.alt-menu').css('width', jQuery('.head__text').position().left);
     } else {
       jQuery('.alt-menu').css('width', '100%');
     }
-  });
-  // initial once
+  }); // initial once
+
   if (jQuery(window).width() > 820) {
     jQuery('.alt-menu').css('width', jQuery('.head__text').position().left);
   } else {
     jQuery('.alt-menu').css('width', '100%');
   }
-
   /**
    * Breadcrumbs
    */
@@ -163,6 +160,8 @@ jQuery(() => {
    * put "Home" root Element of breadcrumb for IPOA
    * (breadcrumb for OJS begins at higher entry level)
    */
+
+
   if (jQuery('li.crumb').first().text() === 'IPOA') {
     jQuery('li.crumb').first().text('Home');
   }
--- build.js
+++ build.js
@@ -12,6 +12,7 @@ buildify()
   .concat([
     'node_modules/jquery/dist/jquery.min.js',
     'Resources/Public/JavaScript/html5shiv.js',
-    'Resources/Public/JavaScript/picturefill.min.js'
+    'Resources/Public/JavaScript/picturefill.min.js',
+    'Resources/Public/JavaScript/bootstrap.min.js'
   ])
   .save('Resources/Public/JavaScript/vendor.min.js');
